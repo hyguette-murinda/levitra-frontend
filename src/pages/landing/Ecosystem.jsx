2d55ece21902e2afd76a2297745d45426aef19ae
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-05-23 21:26:43 +0200

    adding padding

--- src/pages/landing/Ecosystem.jsx
+++ src/pages/landing/Ecosystem.jsx
@@ -7,8 +7,26 @@ function Ecosystem() {
   return (
     <div className="w-full">
       <Navigation link={'ecosystem'} title="Levitra - Ecosystem"/>
-      <div className='flex justify-around'>
-        
+      <div className='flex pt-[6rem] justify-around'>
+        <div>
+          <div>
+          We provide all aspects of medical practice for
+          you whole family, including general check-ups 
+          or assisting you with specific injuries.
+          </div>
+          <div>
+          We'll  work with you to develop individualised
+care plans, including management of
+ chronic diseases. If we cannot assist,
+ we can provide referrals  or advice
+ about type of practitioner   
+ require. We treat all enquires sensitively
+ and in the strictest confidence
+
+          </div>
+        </div>
+        <div></div>
+        <div></div>
       </div>
       <Bottomer />
     </div>
