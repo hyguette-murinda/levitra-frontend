8266db6d019d60a62e992c9207392fb3dd3f50bc
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-05-27 10:25:56 +0200

    adding my loader

--- src/loading/Landing.jsx
+++ src/loading/Landing.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Logo from '../assets/favicon.png'
+import './styles.css'
+
 function Landing() {
   const [load, setLoad] = useState(false);
   const [rot, setRot] = useState(false);
@@ -12,7 +14,7 @@ useEffect(()=>{
   
   return (
     <div style={{zIndex: 50}} className={`flex items-center bg-white justify-center fixed h-[100vh] w-full`}>
-        <div className={`flex items-center gap-3 ${!rot && `overflow-hidden`} duration-1000 ${load ? "w-[11rem]" : 'w-[3rem]'}`}><img src={Logo} alt="logo" className={`h-[3rem] ${rot &&  `animate-bounce`}`}/><div className='text-3xl font-bold text-[#1a489d]'>Levitra</div></div>
+        <div className={`flex items-center gap-3 ${!rot && `overflow-hidden`} duration-1000 ${load ? "w-[11rem]" : 'w-[3rem]'}`}><img src={Logo} alt="logo" className={`h-[3rem] ${rot &&  `spin`}`}/><div className='text-3xl font-bold text-[#1a489d]'>Levitra</div></div>
     </div>
   )
 }
