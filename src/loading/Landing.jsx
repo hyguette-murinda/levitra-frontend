edabdbb2ee014486f0a79ad4a62f9c8637648ba2
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-05-27 10:13:51 +0200

    adding my loader

--- src/loading/Landing.jsx
+++ src/loading/Landing.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from '../assets/favicon.png'
+import './styles.css'
+
 function Landing() {
-  const [load, setLoad] = useState(false)
+  const [load, setLoad] = useState(false);
+  const [rot, setRot] = useState(false)
+useEffect(()=>{
+  setLoad(true);
   setTimeout(()=>{
-      setLoad(true)
-  },100)
+    setRot(true)
+  },1000) 
+},[])
+  
   return (
-    <div style={{zIndex: 50}} className='flex items-center bg-white justify-center fixed h-[100vh] w-full'>
-        <div className={`flex items-center gap-3 overflow-hidden duration-1000 ${load ? "w-[11rem]" : 'w-[3rem]'}`}><img src={Logo} alt="logo" className='h-[3rem]'/><div className='text-3xl font-bold text-[#1a489d]'>Levitra</div></div>
+    <div style={{zIndex: 50}} className={`flex items-center bg-white justify-center fixed h-[100vh] w-full`}>
+        <div id="dur" className={`flex items-center relative gap-3 ${!rot && `overflow-hidden`} duration-1000 ${load ? "w-[11rem]" : 'w-[3rem]'}`}><img src={Logo} alt="logo" className={`h-[3rem] ${rot && 'spin'}`}/><div className='text-3xl font-bold text-[#1a489d]'>Levitra</div></div>
     </div>
   )
 }
