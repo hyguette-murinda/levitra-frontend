1f7aaa852b236fad723d4df30727b98673287c76
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-05-27 10:36:48 +0200

    adding my loader

--- src/loading/Landing.jsx
+++ src/loading/Landing.jsx
@@ -14,7 +14,7 @@ useEffect(()=>{
   
   return (
     <div style={{zIndex: 50}} className={`flex items-center bg-white justify-center fixed h-[100vh] w-full`}>
-        <div className={`flex items-center gap-3 ${!rot && `overflow-hidden`} duration-1000 ${load ? "w-[11rem]" : 'w-[3rem]'}`}><img src={Logo} alt="logo" className={`h-[3rem] ${rot &&  `spin`}`}/><div className='text-3xl font-bold text-[#1a489d]'>Levitra</div></div>
+        <div className={`flex items-center gap-3 ${!rot && `overflow-hidden`} duration-1000 ${load ? "w-[11rem]" : 'w-[3rem]'}`}><img src={Logo} alt="logo" className={`h-[3rem] ${rot &&  'bounce'}`}/><div className='text-3xl font-bold text-[#1a489d]'>Levitra</div></div>
     </div>
   )
 }
